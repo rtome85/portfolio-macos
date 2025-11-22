a08d25a492e3141ed94613f976aec50d742534ae
rtome85 <rob.tome@gmail.com>
2025-11-22 15:03:13 +0000

    Implement Window Store

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,12 +1,25 @@
 import { dockApps } from "#constants";
+import useWindowStore from "#store/window";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React, { useRef } from "react";
 import { Tooltip } from "react-tooltip";
 
 const Dock = () => {
+	const { openWindow, closeWindow, windows } = useWindowStore();
 	const dockRef = useRef(null);
-	const toggleApp = (app) => {};
+
+	const toggleApp = (app) => {
+		if (!app.canOpen) return;
+
+		const window = windows[app.id];
+
+		if (window?.isOpen) {
+			closeWindow(app.id);
+		} else {
+			openWindow(app.id);
+		}
+	};
 
 	useGSAP(() => {
 		const dock = dockRef.current;
