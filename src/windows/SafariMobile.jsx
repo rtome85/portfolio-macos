2482c583af356db28dea3663e4b43e830860e1ab
rtome85 <rob.tome@gmail.com>
2025-12-01 22:37:47 +0000

    Fix footer positioning from absolute to sticky

--- src/windows/SafariMobile.jsx
+++ src/windows/SafariMobile.jsx
@@ -20,11 +20,14 @@ const SafariMobile = () => {
 				<MobileControls target="safariMobile" />
 				<h2 className="text-lg font-bold text-center">Safari</h2>
 			</div>
-			<div className="blog">
+			<div className="blog overflow-y-auto pb-40">
 				<h2>My latest articles</h2>
 				<ul className="flex flex-col gap-4 mb-8">
 					{blogPosts.map(({ id, date, title, image, link }) => (
-						<li key={id} className="flex flex-row gap-4 items-center">
+						<li
+							key={id}
+							className="flex flex-row gap-2 items-center border-b border-gray-200 pb-2 last:border-b-0"
+						>
 							<div className="flex flex-col gap-2">
 								<p className="text-xs text-gray-500">{date}</p>
 								<h3 className="text-base font-semibold text-gray-800">
@@ -52,7 +55,7 @@ const SafariMobile = () => {
 					See all articles
 				</a>
 			</div>
-			<footer className="absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center p-4 border-t border-gray-200 bg-gray-50">
+			<footer className="sticky bottom-0 left-0 right-0 flex flex-col items-center justify-center p-4 border-t border-gray-200 bg-gray-50">
 				<div className="flex flex-row gap-3 items-center bg-white shadow-md rounded-lg px-3 py-3 w-full mb-4">
 					<Search className="size-5 text-gray-400" />
 					<input
